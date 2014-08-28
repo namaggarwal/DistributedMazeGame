d07aacc1bc3585ce5adc1d3f5aaf6927ef72a3ea
namaggarwal <nam.aggarwal@yahoo.com>
2014-08-28 18:53:41 +0800

    Data transfer added

--- src/client/MazeClient.java
+++ src/client/MazeClient.java
@@ -1,6 +1,9 @@
 package client;
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class MazeClient {
@@ -18,10 +21,47 @@ public class MazeClient {
 		int port = Integer.parseInt(args[1]);
 		
 		try{
+			
 			System.out.println("Connecting to server....");
 			Socket ss = new Socket(host,port);
 			System.out.println("Connected to server");
+			
+			//To write to server socket
+			PrintWriter out = new PrintWriter(ss.getOutputStream(),true);
+			
+			//To get input from server socket
+			BufferedReader in = new BufferedReader(new InputStreamReader(ss.getInputStream()));
+			
+			//To get input from user keyboard
+			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+			
+			String fromServer;
+			
+			String fromClient;
+									
+			while((fromServer = in.readLine()) != null){
+							
+				System.out.println("Server says : "+fromServer);
+				
+				if(fromServer.equals("Bye")){
+					
+					break;
+				}
+				
+				fromClient = stdIn.readLine();
+				
+				if(fromClient != null){
+					
+					System.out.println("Client says " + fromClient);
+                    out.println(fromClient);
+				}
+				
+				
+				
+			}			
+			
 			ss.close();
+			
 		}catch(Exception e){
 			
 			System.out.println("Error in connecting to server. Please check if server is running.");
--- src/server/MazeServer.java
+++ src/server/MazeServer.java
@@ -1,6 +1,9 @@
 package server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -9,7 +12,9 @@ public class MazeServer {
 	
 	private void start(int port){
 		
+		//Set this variable to false to stop the server
 		boolean listening = true;
+		
 		try{
 			
 			System.out.println("Starting server on port "+port);
@@ -26,15 +31,41 @@ public class MazeServer {
 				System.out.println("Client connected.");
 				System.out.println("Client host "+MazeClient.getInetAddress());
 				System.out.println("Client port "+MazeClient.getPort());
+				
+				//To write to client socket
+				PrintWriter out = new PrintWriter(MazeClient.getOutputStream(),true);
+			
+				//To read from client socket
+				BufferedReader in = new BufferedReader(new InputStreamReader(MazeClient.getInputStream()));
+							
+				String clientInput;
+			
+				out.println("Connected");
+				
+				while((clientInput = in.readLine()) != null ){
+					
+					System.out.println("Client says : "+clientInput);
+					out.println(clientInput);
+					if(clientInput.equals("Bye")){
+						break;
+					}
+					
+					
+				}
+				
+				// Close the socket connection
+				ss.close();
+				
 			}
 			
-			ss.close();
+			
 			
 			
 		}catch(IOException e){
 			
 			System.err.println("Could not listen on port " + port);
             System.exit(-1);
+            
 		}finally{
 			
 			
